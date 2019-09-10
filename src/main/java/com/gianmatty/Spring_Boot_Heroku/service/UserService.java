47ba138ac4a8603e239742f45a999743b0f458f1
Giancarlo <matiasmauriciogiancarlo@gmail.com>
2019-09-10 15:28:16 -0500

    Implementacion de Update User. Close #6

--- src/main/java/com/gianmatty/Spring_Boot_Heroku/service/UserService.java
+++ src/main/java/com/gianmatty/Spring_Boot_Heroku/service/UserService.java
@@ -6,4 +6,8 @@ public interface UserService {
     public Iterable<User> getAllUsers();
 
     public User createUser(User user) throws Exception;
+
+    public User getUserById(Long id) throws Exception;
+
+    public User updateUser(User user) throws Exception;
 }
--- src/main/java/com/gianmatty/Spring_Boot_Heroku/service/UserServiceImpl.java
+++ src/main/java/com/gianmatty/Spring_Boot_Heroku/service/UserServiceImpl.java
@@ -43,4 +43,29 @@ public class UserServiceImpl implements UserService{
         return user;
     }
 
+    @Override
+    public User getUserById(Long id) throws Exception {
+        return repository.findById(id).orElseThrow(() -> new Exception("El usuario para editar no existe."));
+    }
+
+    @Override
+    public User updateUser(User fromUser) throws Exception {
+        User toUser = getUserById(fromUser.getId());
+        mapUser(fromUser, toUser);
+        return repository.save(toUser);
+    }
+
+    /**
+     * Map everythin but the password.
+     * @param from
+     * @param to
+     */
+    protected void mapUser(User from,User to) {
+        to.setUsername(from.getUsername());
+        to.setFirstName(from.getFirstName());
+        to.setLastName(from.getLastName());
+        to.setEmail(from.getEmail());
+        to.setRoles(from.getRoles());
+    }
+
 }
