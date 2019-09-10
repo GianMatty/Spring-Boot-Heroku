39b3ddfee075504ba693d0a6c598b4ce87ffd0ec
Giancarlo <matiasmauriciogiancarlo@gmail.com>
2019-09-10 07:58:43 -0500

    Commit inicial con IntelliJ IDEA. Close #1

---
+++ src/main/java/com/gianmatty/Spring_Boot_Heroku/SpringBootHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.gianmatty.Spring_Boot_Heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHerokuApplication.class, args);
+	}
+
+}
